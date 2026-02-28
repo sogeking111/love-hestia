30162595386f8525ff56ce45407e242f100e8385
Keno Duane <k.duane@ibs-dev.de>
2026-02-28 17:27:03 +0800

    added token for products api

--- lh-online-ordering/src/services/product-service.js
+++ lh-online-ordering/src/services/product-service.js
@@ -1,8 +1,31 @@
 import api from "./api";
 
+// --- Get JWT Token ---
+async function getToken() {
+  try {
+    const res = await axios.post(
+      `${API_HOST}/jwt-auth/v1/token`,
+      {
+        username: "admin",
+        password: "admin",
+      },
+      { headers: { "Content-Type": "application/json" } },
+    );
+    return res.data.token;
+  } catch (err) {
+    console.error("JWT Token Error:", err.response?.data || err.message);
+    throw err;
+  }
+}
+
 export const productService = {
   async getAll() {
+    const token = await getToken();
+
     const res = await api.get("/product", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
       params: {
         _embed: true,
         per_page: 100, // adjust if needed
